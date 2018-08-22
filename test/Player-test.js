a6315dbefbd699d3124c670e0e439a08bc310e92
Graham <g@gmasterofnone.com>
2018-08-22 12:31:23 -0600

    Add final Player test

--- test/Player-test.js
+++ test/Player-test.js
@@ -10,6 +10,22 @@ describe('Player', () => {
 
   })
 
+  it('should take properties', () => {
+    player = new Player(26, 270, 10, 10, 'green', 1)
+    assert.deepEqual(player, {
+      x: 26,
+      y: 270,
+      height: 10,
+      width: 10,
+      color: 'green',
+      dx: 1,
+      dy: 0,
+      dxv: 3,
+      dyv: 3,
+      score: 3
+    })
+  })
+
   it('When player dies their score should decrease', () => {
     player = new Player(26, 270, 10, 10, 'green', 1, 0)
 
